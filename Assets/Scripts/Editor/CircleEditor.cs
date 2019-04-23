e4babedf13c63897f26ee004572e8ef2c4663b23
JutalP <victor.jespersen@hotmail.se>
2019-04-23 16:59:50 +0200

    Custom Window Editor
    Added a Window Custom Editor to the circle settings including vertexCount and radius

---
+++ Assets/Scripts/Editor/CircleEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+class MyWindow : Editor
+{
+    SerializedProperty vertexCount;
+
+    [MenuItem("Window/My Window")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CircleWindow));
+    }
+
+
+    private void OnEnable()
+    {
+        vertexCount = serializedObject.FindProperty("vertexCount");
+    }
+
+    void OnGUI()
+    {
+        // The actual window code goes here
+    }
+}
---
+++ Assets/Scripts/Editor/CircleWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CircleWindow : EditorWindow
+{
+    Circle MyCircle;
+    public float myVertexFloat;
+    public float myRadiusFloat;
+
+    private void Awake()
+    {
+        MyCircle = FindObjectOfType<Circle>();
+    }
+
+    [MenuItem("Window/CircleWindow")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CircleWindow));
+    }
+
+    private void OnGUI()
+    {
+        myVertexFloat = EditorGUILayout.Slider("VertexCount", MyCircle.vertexCount, 3, 40);
+        myRadiusFloat = EditorGUILayout.Slider("Radius", MyCircle.radius, 1, 100);
+        MyCircle.radius = myRadiusFloat;
+        //MyCircle.vertexCount = myVertexFloat;
+    }
+}
